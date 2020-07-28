54b92430ff106d1e5efacb232ba842e86778157a
Takeshi <igorsilva0017@gmail.com>
2020-07-28 18:25:35 -0300

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App'
+
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import CadastroVideo from './pages/cadastro/Video'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+  
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={() => (<div>404 Not found</div>)}/>
+
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
