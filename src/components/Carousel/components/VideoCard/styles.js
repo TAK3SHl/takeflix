7f803dcbc8a077aac2015ff13a6896605f702f67
Takeshi <igorsilva0017@gmail.com>
2020-07-28 21:06:44 -0300

    Personalizações Pessoais

--- src/components/Carousel/components/VideoCard/styles.js
+++ src/components/Carousel/components/VideoCard/styles.js
@@ -1,19 +1,17 @@
 import styled from 'styled-components';
 
 export const VideoCardContainer = styled.a`
-  border: 2px solid;
-  border-radius: 4px;
+  border-radius: 1px;
   text-decoration: none;
   overflow: hidden;
   cursor: pointer;
   color: white;
   flex: 0 0 298px;
   width: 298px;
-  height: 197px;
+  height: 168px;
   background-image: ${({ url }) => `url(${url})`};
   background-size: cover;
   background-position: center;
-  border-radius: 10px;
   position: relative;
   display: flex;
   align-items: flex-end;
@@ -23,6 +21,8 @@ export const VideoCardContainer = styled.a`
   &:hover,
   &:focus {
     opacity: .5;
+    transform: scale(0.98);
+    transition: 0.5s
   }
   
   &:not(:first-child) {
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -6,8 +6,9 @@ export const FooterBase = styled.footer`
   border-top: 2px solid red;
   padding-left: 16px;
   padding-right: 16px;
-  padding-top: 32px;
-  padding-bottom: 32px;
+  padding-top: 24px;
+  padding-bottom: 15px;
+  margin-top: 100px;
   color: var(--white);
   text-align: center;
   @media (max-width: 800px) {
